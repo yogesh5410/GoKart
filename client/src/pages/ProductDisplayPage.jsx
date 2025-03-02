08a6ec44755f3c3fb85975583df3fed2b50eeeac
yogesh5410 <yogesh5410official@gmail.com>
2025-03-02 20:35:33 +0530

    version 1.4

--- client/src/pages/ProductDisplayPage.jsx
+++ client/src/pages/ProductDisplayPage.jsx
@@ -9,7 +9,7 @@ import Divider from '../components/Divider'
 import image1 from '../assets/minute_delivery.png'
 import image2 from '../assets/Best_Prices_Offers.png'
 import image3 from '../assets/Wide_Assortment.png'
-import { pricewithDiscount } from '../utils/pricewithDiscount'
+import { pricewithDiscount } from '../utils/PriceWithDiscount'
 import AddToCartButton from '../components/AddToCartButton'
 
 const ProductDisplayPage = () => {
